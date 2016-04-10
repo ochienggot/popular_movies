1b122743b22f3299c296c1efd7fb0e37615f47de
Nicholas Got <nicholas.got@gmail.com>
2016-04-10 14:07:37 +0200

    Update FetchMovieDetails.java

--- app/src/main/java/com/nicholasgot/project1/FetchMovieDetails.java
+++ app/src/main/java/com/nicholasgot/project1/FetchMovieDetails.java
@@ -46,7 +46,7 @@ public class FetchMovieDetails {
         urlBuilder.addPathSegment("3");
         urlBuilder.addPathSegment("movie");
         urlBuilder.addPathSegment(id);
-        urlBuilder.addQueryParameter(API_KEY, "f02800d89481918a2f7b70b9375ed8ad");
+        urlBuilder.addQueryParameter(API_KEY, "API KEY");
         String okUrl = urlBuilder.build().toString();
 
         Request request = new Request.Builder()
